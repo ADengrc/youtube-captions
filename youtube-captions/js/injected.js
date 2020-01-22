e7de7d950acd9ae3d99c8bf6379928c57ca4d945
dengrc <624616667@qq.com>
2020-01-22 19:34:41 +0800

    [!]处理youtube字幕接口更新导致插件不能使用的bug

--- youtube-captions/js/injected.js
+++ youtube-captions/js/injected.js
@@ -1,43 +1,90 @@
-xhook.after(function (request, response) {
-    let url = request.url
-    if (url.includes('/api/timedtext')) {
-        const zhReg = /^zh-\w+/
-        const params = new URLSearchParams(url)
-        let lang = (params.get('lang') || '').toLocaleLowerCase()
-        let tlang = (params.get('tlang') || '').toLocaleLowerCase()
-        
-        if (!zhReg.test(lang) && !zhReg.test(tlang)) {
-            let userLang
-            try {
-                JSON.parse(ytplayer.config.args.player_response).captions.playerCaptionsTracklistRenderer.captionTracks.forEach(lang => {
-                    lang.languageCode = lang.languageCode.toLocaleLowerCase()
-                    if (userLang && userLang.languageCode == 'zh-cn') return
-                    if (zhReg.test(lang.languageCode)) {
-                        lang.baseUrl += '&fmt=srv3'
-                        userLang = lang
-                    }
-                })
-            } catch (error) {
-                console.log(error)
-            }
-
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', userLang ? userLang.baseUrl : `${url}&tlang=zh-Hans`, false);
-            xhr.send();
-
-            xhr.responseXML.querySelectorAll('p').forEach(e => {
-                let p = response.xml.querySelector(`p[t='${e.getAttribute('t')}']`);
-                if (p) {
-                    if (p.childElementCount && e.previousElementSibling) {
-                        let previous = e.previousElementSibling;
-                        previous.setAttribute('d', e.getAttribute('t') - previous.getAttribute('t'));
-                    }
-
-                    e.textContent = [p.textContent.replace('\n', ' '), e.textContent.replace('\n', ' ')].join('\n');
-                }
-            });
-
-            response.text = new XMLSerializer().serializeToString(xhr.responseXML);
-        }
+let mergerSegs = function(segs, event, map) {
+  if (segs) {
+    let utf8 = segs.map(seg => seg.utf8).join("");
+    let val = map && map.get(`${event.tStartMs}_${event.dDurationMs}`);
+    if (val) {
+      utf8 = `${utf8}\n${val[0].utf8}`;
     }
+    return [
+      {
+        utf8
+      }
+    ];
+  } else {
+    return [
+      {
+        utf8: ""
+      }
+    ];
+  }
+};
+
+let setMap = function(userLang, url) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", userLang ? userLang.baseUrl : `${url}&tlang=zh-Hans`, false);
+  xhr.send();
+  let map = new Map();
+  JSON.parse(xhr.response).events.forEach(event => {
+    if (event.segs) {
+      map.set(`${event.tStartMs}_${event.dDurationMs}`, mergerSegs(event.segs));
+    }
+  });
+  return map;
+};
+
+let getResult = function(response, map) {
+  let resJson = JSON.parse(response.text);
+  let events = [];
+  resJson.events.forEach(function(event) {
+    if (
+      !(event.segs && event.segs.length === 1 && event.segs[0].utf8 === "\n")
+    ) {
+      if (event && event.segs) {
+        event.segs = mergerSegs(event.segs, event, map);
+      }
+      events.push(event);
+    }
+  });
+  resJson.events = events;
+  return JSON.stringify(resJson);
+};
+
+let injectedStyle = function() {
+  let style = document.createElement("style");
+  style.innerText =
+    ".caption-window.ytp-caption-window-bottom{transform:scale(2) translateY(-30%)}";
+  document.head.appendChild(style);
+};
+injectedStyle();
+
+xhook.after(function(request, response) {
+  let url = request.url;
+  if (url.includes("/api/timedtext")) {
+    const zhReg = /^zh-\w+/;
+    const params = new URLSearchParams(url);
+    let lang = (params.get("lang") || "").toLocaleLowerCase();
+    let tlang = (params.get("tlang") || "").toLocaleLowerCase();
+
+    if (!zhReg.test(lang) && !zhReg.test(tlang)) {
+      let userLang;
+      try {
+        JSON.parse(
+          ytplayer.config.args.player_response
+        ).captions.playerCaptionsTracklistRenderer.captionTracks.forEach(
+          lang => {
+            lang.languageCode = lang.languageCode.toLocaleLowerCase();
+            if (userLang && userLang.languageCode == "zh-cn") return;
+            if (zhReg.test(lang.languageCode)) {
+              lang.baseUrl += "&fmt=srv3";
+              userLang = lang;
+            }
+          }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+      let map = setMap(userLang, url);
+      response.text = getResult(response, map);
+    }
+  }
 });
